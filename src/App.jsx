88d50f3b0df006a3d3ebfe530f3aabbc18a87a2e
WebWithAstha <lodhiastha85@gmail.com>
2024-05-01 21:21:15 +0530

    2 way binding learned for each type of form inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = ()=>{
 
-  return (
+  const [username,setusername]=useState('astha')
+  const [type,settype]=useState('coder')
+  const [age,setage]=useState('18')
+  const [ans,setans]=useState('')
+  const [text,settext]=useState('hello bhai likho mita k')
+
+  console.log(ans)
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>React Form</h1>
+    <hr />
+    <form action="">
+      <input onChange={(e)=>{setusername(e.target.value)}} value={username} type="text" name="username" placeholder="username" /><br /><br />
+      <input value={ans?"yes":"no"} onChange={(e)=>{setans(e.target.checked)}} checked={ans?true:false} type="checkbox" name="" id="" value="yes" />
+      <label htmlFor="">yes</label><br /><br />
+      <input onChange={(e)=>{settype(e.target.value)}} type="radio" checked={type === "coder"?true:false} value="coder" />
+      <label htmlFor="">coder</label>
+      <input onChange={(e)=>{settype(e.target.value)}} type="radio" checked={type === "non-coder"?true:false} value="non-coder" />
+      <label htmlFor="">non-coder</label><br /><br />
+      <textarea onChange={(e)=>{settext(e.target.value)}} name="description" id="" cols="30" rows="1">{text}</textarea><br /><br />
+      <select defaultValue={age} onChange={(e)=>{setage(e.target.value)}} name="age" id="">
+        <option value="18">18</option>
+        <option value="20">20</option>
+      </select><br /><br />
+      <button>submit</button><br /><br />
+      <hr />
+    </form>
     </>
   )
-}
 
+}
 export default App
